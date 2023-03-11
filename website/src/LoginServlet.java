fc072cc9240091f90a4cfa8823d02a0b4fd0cd89
sky <skyhaihai2000@gmail.com>
2023-03-10 23:12:44 -0500

    iteration 1 finished
    basic login logic achieved

--- website/src/LoginServlet.java
+++ website/src/LoginServlet.java
@@ -1,16 +1,20 @@
 import java.io.*;
 import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
-
-		if (username == "admin" || password == "admin") {
+		
+		if ("admin".equals(username) && "admin".equals(password)) {
 			response.sendRedirect("welcome.jsp");
+		}else{
+			response.sendRedirect("index.jsp");
 		}
 	}
 
